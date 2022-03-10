316b4e15d6cbc10a4294e004bf6afd6cf2af101e
AMW <73808213+AntonTech420@users.noreply.github.com>
2022-03-11 11:29:29 +1300

    Update #Web Scraper.py
    Added code

--- #Web Scraper.py
+++ #Web Scraper.py
@@ -1,3 +1,32 @@
 #Web Scraper
 
-#This is linked to github
+import requests 
+from bs4 import BeautifulSoup
+
+global info_list
+global nlp
+info_list=[]
+nlp=[]
+
+def scrape_info(url): 
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    info_items = soup.find_all('div', class_='listing_content')
+    for accommodations in info_items: 
+        address = accommodations.find('p', class_='address')
+        try: 
+            if address is not None: 
+                info_list.append([address.text])
+                details = address.text
+                i, a_phone = details.split('+', 1)
+                a_name, a_location = i.split(', ', 1)
+                nlp.append([a_name, a_location, a_phone])
+        except: 
+            pass
+    print(nlp)
+catgory = input("Enter Category name")
+pageNum=["","-2","-3","-4","-5","-6","-7","-8"] 
+
+for i in pageNum:
+    scrape_info("https://www.nzdirectory.co.nz/"+str(catgory) +str(i)+".html")
+
